d302bf9018b65d2de1a727db24e52cba19614e5c
Jayson Asiado <118736660+s0y4hh@users.noreply.github.com>
2023-09-13 16:24:38 +0800

    Add files via upload

---
+++ ASIADO/Act1_Jayson.java
@@ -0,0 +1,13 @@
+// a program that prints student information
+public class Act1_Jayson{
+	public static void main (String[] args){
+		System.out.print("Student Name: Jayson Asiado"
+		+ "\nStudent Id: 221000898"
+		+ "\nAge: 20"
+		+ "\nGender: Male"
+		+ "\nAddress: Libmanan Camarines Sur"
+		+ "\nSchool: Camarines Sur Polytechnic Colleges"
+		+ "\nProgram: BSCS"
+		+ "\nYear Level: 2nd Year");
+	}
+}
---
+++ ASIADO/Hello.java
@@ -0,0 +1,6 @@
+// A program that prints Hello World!
+public class Hello{
+	public static void main (String[] args){
+		System.out.println("Hello, World!");
+	}
+}
